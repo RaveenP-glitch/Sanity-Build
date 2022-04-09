c1cb9ec0bc4f705aabb3570e17e1129568e36833
Raveen Panditha <raveenpanditha9@gmail.com>
2022-04-09 23:40:56 +0530

    scss lines

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.scss';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
+
+import { images } from '../../constants';
 
 const Navbar = () => {
+
+    const [toggle, setToggle] = useState(false);
+
   return (
-    <nav>
-        <div>
+    <nav className='app__navbar'>
+        <div className='app__navbar-logo'>
             <img src={images.logo} alt="" />
         </div>
+        <ul className='app__navbar-links'>
+            { ['home', 'about', 'work', 'skills', 'contact'].map((item)=>(
+                <li className='app__flex p-text' key={`link-${item}`}>
+                    <div />
+                    <a href={`#${item}`}>{item}</a>
+                </li>
+            )) }
+        </ul>
+
+        <div className='app__navbar-menu'>
+            <HiMenuAlt4 onClick={() => setToggle(false)} />
+
+
+        </div>
+
     </nav>
   )
 }
