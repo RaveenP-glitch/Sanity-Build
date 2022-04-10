99420a05cad2d0d70de4c49566c4d34e59c2baf6
Raveen Panditha <raveenpanditha9@gmail.com>
2022-04-10 15:23:36 +0530

    About section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,10 +1,52 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 import './About.scss';
+
+const abouts =[
+  {title: 'Web Development',
+    description: 'I am a good Web Developer.',
+    imgUrl: ''  
+  },
+  {title: 'Web Design',
+    description: 'I am a good Web Developer.',
+    imgUrl: ''  
+  },
+  {title: 'UI/UX',
+    description: 'I am a good Web Developer.',
+    imgUrl: ''  
+  },
+  {title: 'Web Animations',
+    description: 'I am a good Web Developer.',
+    imgUrl: ''  
+  }
+]
 
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+      <h2 className='head-text'>
+        I Know That
+        <span>Good Design</span>
+        <br/>
+        means
+        <span>Good Business</span>
+       </h2>
+
+       <div className='app__profiles'>
+        {abouts.map((about,index)=> (
+          <motion.div
+            whileInView={{ opacity:1 }}
+            whileHover={{ scale: 1.1 }}
+            transition={{ duration: 0.5, type: 'tween' }}
+          >
+
+
+          </motion.div>
+        ))}
+
+       </div>
+    </>
   )
 }
 
