6fb950984a848eb8c5c7848b9b09a74235df64f4
Raveen Panditha <raveenpanditha9@gmail.com>
2022-04-11 00:44:29 +0530

    client files

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion';
 import './About.scss';
 
 import { images } from '../../constants';
+import { urlFor,client } from '../../client';
 
 const abouts =[
   {title: 'Web Development',
@@ -25,6 +26,14 @@ const abouts =[
 
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type'
+  }, [])
+  
+
   return (
     <>
       <h2 className='head-text'>I Know That <span>Good Development</span><br/>means <span>Good Business</span></h2>
