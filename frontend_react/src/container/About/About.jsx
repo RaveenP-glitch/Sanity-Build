b3fbdf6741ae078fea14938b13065b31676154a0
Raveen Panditha <raveenpanditha9@gmail.com>
2022-04-11 23:37:46 +0530

    newlines

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -5,24 +5,24 @@ import './About.scss';
 import { images } from '../../constants';
 import { urlFor,client } from '../../client';
 
-const abouts =[
-  {title: 'Web Development',
-    description: 'I am a good Web Developer.',
-    imgUrl: images.about01  
-  },
-  {title: 'Front-end Development',
-    description: 'I am a good Web Developer.',
-    imgUrl: images.about02  
-  },
-  {title: 'UI/UX',
-    description: 'I am a good Web Developer.',
-    imgUrl: images.about03   
-  },
-  {title: 'MERN Stack',
-    description: 'I am a good Web Developer.',
-    imgUrl: images.about04   
-  }
-]
+// const abouts =[
+//   {title: 'Web Development',
+//     description: 'I am a good Web Developer.',
+//     imgUrl: images.about01  
+//   },
+//   {title: 'Front-end Development',
+//     description: 'I am a good Web Developer.',
+//     imgUrl: images.about02  
+//   },
+//   {title: 'UI/UX',
+//     description: 'I am a good Web Developer.',
+//     imgUrl: images.about03   
+//   },
+//   {title: 'MERN Stack',
+//     description: 'I am a good Web Developer.',
+//     imgUrl: images.about04   
+//   }
+// ]
 
 
 const About = () => {
@@ -30,7 +30,12 @@ const About = () => {
   const [abouts, setAbouts] = useState([]);
 
   useEffect(() => {
-    const query = '*[_type'
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query)
+    .then((data)=> setAbouts(data))
+
+
   }, [])
   
 
@@ -47,7 +52,7 @@ const About = () => {
             className="app__profile-item"
             key={about.title + index}
           >
-            <img src={about.imgUrl} alt="about.title" />
+            <img src={urlFor(about.imgUrl)} alt="about.title" />
             <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
             <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
 
