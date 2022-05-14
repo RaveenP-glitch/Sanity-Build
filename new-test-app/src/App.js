49cd25f96a129ec80f21b997584dcb738ff852b3
Raveen Panditha <raveenpanditha9@gmail.com>
2022-05-15 01:00:39 +0530

    new changes

--- new-test-app/src/App.js
+++ new-test-app/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      const [cTime, setTime] = useState(new Date().toLocaleString());
+      useEffect(()=>{
+        //
+        setInterval(()=>{
+          setTime(new Date().toLocaleString());
+          
+        },1000);
+
+      })
+      return(
+        <div className="App">
+          <h1>{cTime}</h1>
+          <button onClick={()=>setTime(new Date().toLocaleString())}>Increment</button>
+
+
+        </div>
+      )
+
+
     </div>
   );
 }
